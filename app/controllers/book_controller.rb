7ed9abe00d848d6dde7059b7f65d3f60b4f91fe4
Apprentice <philborel21@gmail.com>
2015-08-15 16:09:54 -0700

    fix typos in book_controller.rb - add dos

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,15 +1,16 @@
-get '/users/:user_id/books'
+get '/users/:user_id/books' do
   erb :"books/index"
 end
 
-get '/users/:user_id/books/:id'
+get '/users/:user_id/books/:id' do
   erb :"books/book"
 end
 
-put '/users/:user_id/books/:id'
-  Book.update(params[:id],
-              playlists: params[:playlist],
-              comments: params[:comment])
+put '/users/:user_id/books/:id' do
+  # Book.update(params[:id],
+              # user_id: params[:user_id],
+              # playlists: params[:playlist],
+              # comments: params[:comment])
 
   erb :"books/book"
 end
