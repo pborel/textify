1da0f32401b6e89832cbbe771c31a47e8d78772e
Apprentice <philborel21@gmail.com>
2015-08-16 10:42:27 -0700

    add ajax to comments

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,48 @@
 $(document).ready(function() {
-//   // This is called after the document has loaded in its entirety
-//   // This guarantees that any elements we bind to will exist on the page
-//   // when we try to bind to them
   replaceForm()
 //   signupToggle();
-//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  bindListeners()
 });
 
+var bindListeners = function() {
+  $( '#comment' ).on('reset', function () {
+    event.preventDefault()
+    createNewComment();
+  })
 
-var replaceForm = function(){
-
-  $('#submitbutton').click(function(){
-     $('.formReplace1').replaceWith("<form action='/signup' method='post'>");
-     $('.formReplace2').replaceWith("<input type='text' name='email' id='email' required>");
+  $('#submitbutton').click(function() {
+    event.preventDefault();
+    replaceForm();
   })
 }
+
+var replaceForm = function(){
+   $('.formReplace1').replaceWith("<form action='/signup' method='post'>");
+   $('.formReplace2').replaceWith("<input type='text' name='email' id='email' required>");
+ }
 // func signupToggle = function(){
 //   $('button.signupbutton').click(function(){
 //     $('#signup').toggle();
 //   })
 // }
+
+var createNewComment = function() {
+  var url = $( 'form.comment' ).attr('action')
+  var data = $( 'form.comment' ).serialize()
+
+  $.ajax({
+    url: url,
+    type: 'POST',
+    dataType: 'json',
+    data: data
+  })
+  .done(function(response) {
+    displayNewComment(response)
+  })
+}
+
+
+var displayNewComment = function(response) {
+  // var val = document.getElementById('comment').value
+  document.getElementById('comment_list').innerHTML = document.getElementById('comment_list').innerHTML + "<br>" + response.body
+}
