95911bfa123c2c0bded5a8969afc19aba6aa11b1
Phil Borel <email": null>
2015-08-21 08:55:27 -0700

    add seeds, add show route for playlists

--- textify/app/controllers/playlists_controller.rb
+++ textify/app/controllers/playlists_controller.rb
@@ -1,6 +1,13 @@
 class PlaylistsController < ApplicationController
   def index
     @playlists = Playlist.where(user_id: params[:user_id])
+    @id = params[:id]
+  end
+
+  def show
+    @playlists = Playlist.where(user_id: params[:user_id])
+    @id = params[:id]
+    render "index"
   end
 
 end
